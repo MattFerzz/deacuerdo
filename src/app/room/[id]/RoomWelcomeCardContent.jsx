cce62bd4edb2a247e5030eb135270763938c7b3e
jaraJulieta <jarajuli13@gmail.com>
2023-11-22 01:20:14 -0300

    loading en seleccion

--- src/app/room/[id]/RoomWelcomeCardContent.jsx
+++ src/app/room/[id]/RoomWelcomeCardContent.jsx
@@ -3,14 +3,16 @@
 import DecisionRoom from '@/app/models/DecisionRoom'
 import User from '@/app/models/User'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 import {
-  Button, Card, Form, FormGroup, ListGroup,
+  Button, Card, Form, FormGroup, ListGroup, Spinner,
 } from 'react-bootstrap'
 import InputGroup from 'react-bootstrap/InputGroup'
 
 function RoomWelcomeCardContent({ serializedRoom, addUserToRoom }) {
   const router = useRouter()
   const room = DecisionRoom.deserialize(serializedRoom)
+  const [loading, setLoading] = useState(false)
 
   const backgroundColorStyle = {
     backgroundColor: '#9696ffb3',
@@ -19,14 +21,23 @@ function RoomWelcomeCardContent({ serializedRoom, addUserToRoom }) {
 
   const handleSubmit = async (event) => {
     event.preventDefault()
-    const formData = Array.from(event.target.elements).reduce((acc, input) => {
-      acc[input.id] = input.value
-      return acc
-    }, {})
+    try {
+      setLoading(true)
+      const formData = Array.from(event.target.elements).reduce((acc, input) => {
+        acc[input.id] = input.value
+        return acc
+      }, {})
 
-    const user = User.named(formData.userName)
-    await addUserToRoom(user.serialized(), room.serialized())
-    router.push(`${room.id()}/${user.name()}/selection`)
+      const user = User.named(formData.userName)
+      await addUserToRoom(user.serialized(), room.serialized())
+      router.push(`${room.id()}/${user.name()}/selection`)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const handleCancel = () => {
+    router.push('/')
   }
 
   return (
@@ -51,9 +62,27 @@ function RoomWelcomeCardContent({ serializedRoom, addUserToRoom }) {
           </Form.Label>
           <Form.Control placeholder='Ej: Jose Perez' id='userName' />
         </FormGroup>
-        <Button className='float-end' variant='primary' type='submit'>
-          Continuar
-        </Button>
+        <div className='container d-flex justify-content-between'>
+          <Button className='btn btn-danger rounded-pill px-3' type='button' onClick={handleCancel}>
+            Cancelar
+          </Button>
+          <Button className='btn btn-primary rounded-pill px-3' type='submit' disabled={loading}>
+            {loading ? (
+              <>
+                <Spinner
+                  as='span'
+                  animation='border'
+                  size='sm'
+                  role='status'
+                  aria-hidden='true'
+                />
+                <span className='ms-2'>Loading...</span>
+              </>
+            ) : (
+              'Continuar'
+            )}
+          </Button>
+        </div>
       </Form>
     </Card.Body>
   )
